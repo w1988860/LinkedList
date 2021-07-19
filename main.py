19c207d58b83497e59a91726a57cf2357b4e3ea5
NarutoFAN <87558857+w1988860@users.noreply.github.com>
2021-07-19 15:20:48 +0000

    Completed linked lists

--- main.py
+++ main.py
@@ -5,123 +5,172 @@
 
 # Write your code from here
 
-#Version_01
-#Use any visualization tool to understand the flow of execution
-
-class Node:
-    def __init__(self, data, next=None):
+# Created a Node class to insert a node in linkedlist
+class Node :
+    def __init__(self,data,next):
         self.data = data
         self.next = next
 
-##first_element = Node(5)
-##print("Data in the node is : ", first_element.data)
-
-class Linked_List:
+# Created a Linked List class        
+class LinkedList :
+    # A head function for linked lists
     def __init__(self):
         self.head = None
 
 
-my_linked_list = Linked_List()
-##my_linked_list.head = first_element
+## INSERTION FUNCTIONS :
 
-##second_element = Node(9)
-##my_linked_list.head.next = second_element
-##
-####
-####print(my_linked_list.head.data)
-####
-##### Insert element at the beginning
-##
-##element_1 = input("Enter a number to be inserted at the beginning: ")
-##new_node = Node(element_1)
-##my_linked_list.head = new_node
-##new_node.next = first_element
-####
-##### Insert element at the end
-##
-##element_2 = input("Enter a number to be inserted at the end: ")
-##new_node = Node(element_2)
-##temporary_node = my_linked_list.head
-##while temporary_node.next != None:
-##    temporary_node = temporary_node.next
-##temporary_node.next = new_node    
-##    
-##### Insert element at a particular location
-##location = int(input("Enter the position or location U want to insert a new node: "))
-### case where the location value is greater than the size of the linked list is not considered here
-##element_3 = input("Enter a number to be inserted at a particular location: ")
-##new_node = Node(element_3)
-##temporary_node = my_linked_list.head
-##
-##for i in range(location-1):
-##    temporary_node = temporary_node.next
-##
-##new_node.next = temporary_node.next
-##temporary_node.next = new_node
-    
+    # This is a function to insert data list and convert the list into linked-list
+    def insert_list(self, data_list) :
+      self.head = None
+      for data in data_list :
+        self.insert_at_end(data) 
 
-def linkedlist_traversal(head_node):
-    count = 0
-    temporary_node = head_node
-    while temporary_node != None:
-        print(temporary_node.data)
-        temporary_node = temporary_node.next
+    # This function is to insert a Node at the Begining
+    def insert_at_begining(self, data):
+        self.head = Node(data, self.head)
+
+    # This function is to insert a Node at the Ending
+    def insert_at_end(self,data):
+        if self.head == None:
+            self.head = Node(data,self.head)
+            return
+        itr = self.head
+        while itr.next :
+            itr = itr.next
+        itr.next = Node(data,None)
+
+    # This function is to insert a node at a given index   
+    def insert_at (self,index,data) :
+      if index<0 or index >= self.length():
+        raise Exception("Invalid index")
+      if index == 0 :
+        self.insert_at_begining(data)
+        return
+      count = 0 
+      itr = self.head
+      while itr :
+        if count == index-1 :
+          node = Node(data,itr.next) 
+          itr.next = node
+          break 
+        itr = itr.next
         count += 1
-    print("Number of elements in the linked list is : ", count)
+
+
+## DELETTING FUNCTIONS :
+
+    # This function is used to delete node at begining
+    def remove_at_begining (self) :
+        self.head = self.head.next
     
-
-def linkedlist_search(head_node, element):
-    temporary_node = head_node
-    flag = False
-    while temporary_node != None:
-        if temporary_node.data == element:
-            flag = True
-            print("Element found")
-            break
-        temporary_node = temporary_node.next
-    if flag == False:
-        print("Element not found")
+    # This function is used to delete node at end   
+    def remove_at_end (self) :
+        count = 0
+        itr = self.head
+        a = list.length()
+        while itr :
+            if count == a-2 :
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count+=1       
+      
+    # This remove function is used to delete a node at a given index
+    def remove_at (self, index):
+        if index < 0 or index >= self.length():
+            raise Exception("Invalid index")
             
-
-#Version_02
-
-# Create a linked list with 10 nodes for demo purpose
-node_elements = [5,4,3,2,1,6,7,8,9,0]
-
-for i in range(10):
-    new_node = Node(node_elements[i])
-    if my_linked_list.head==None:  # Inserting first element
-        my_linked_list.head = new_node
-    else:
-        temporary_node = my_linked_list.head
-        while temporary_node.next != None:
-            temporary_node = temporary_node.next
-        temporary_node.next = new_node
-
-linkedlist_traversal(my_linked_list.head)
-
-## Search for an element in the linked list
-element = int(input("Enter the number to be searched: "))
-
-linkedlist_search(my_linked_list.head, element)
+        if index == 0:
+            self.head = self.head.next
+            return
         
+        count =0
+        itr = self.head
+        while itr:
+            if count == index-1 :
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count+=1
 
-# Delete an element at the beginning
-my_linked_list.head = my_linked_list.head.next
-linkedlist_traversal(my_linked_list.head)
 
-# Delete an element at the end
-temporary_node = my_linked_list.head
-while temporary_node.next.next != None:
-    temporary_node = temporary_node.next
-temporary_node.next = None    
-linkedlist_traversal(my_linked_list.head)
+## Length and Searching Functions
 
-# Delete an element at a particular location
-position = int(input("Enter at what position U want the element to be deleted: "))
-temporary_node = my_linked_list.head
+    # This funtion is to get the length of data in Linked List
+    def length(self):
+      count=0
+      itr = self.head
+      while itr :
+        count += 1
+        itr = itr.next
+      return count
+    
+    # This function is to search the elements in LinkedList 
+    def Search(self,data) :
+      if self.head.data == data:
+        print(data,"is in LinkedList")
+        return
+      itr = self.head
+      while itr.next:
+          if itr.next.data == data:
+              print(data,"is in LinkedList")
+              return
+          itr = itr.next
+      print(data,"is not found in LinkedList")
 
-for i in range(position-2):
-    temporary_node = temporary_node.next
-temporary_node.next = temporary_node.next.next
-linkedlist_traversal(my_linked_list.head)
+
+## Printing Funtion of linked lists
+
+    # This print function is to print the linked list
+    def print(self) :
+        if self.head is None :
+            print("Linked list is Empty")
+            return 
+        itr = self.head
+        llstr = ''
+        while itr :
+            llstr += str(itr.data) + "-->"
+            itr = itr.next
+        print(llstr)
+
+
+if __name__ == '__main__' :
+    list = LinkedList()
+## Inserting Nodes
+    # Inserting a list of data to make LinkedList
+    list.insert_list(["Banana","Mango","Grapes","Orange"])
+    list.print()
+    print() 
+    # Inserting a Node at Begining of the LinkedList
+    list.insert_at_begining("Begining")
+    list.print()
+    print()
+    # Inserting a Node at end of the LinkedList
+    list.insert_at_end("Ending")
+    list.print()
+    print()
+    # Inserting a Node at the specified index of LinkedList
+    list.insert_at(3, "insert_1")
+    list.print()
+    print()
+## Deleting Node 
+    # Deleting a Node at Begining of the LinkedList 
+    list.remove_at_begining ()
+    list.print()
+    print()
+    # Deleting a Node at end of the LinkedList
+    list.remove_at_end()
+    list.print()
+    print()
+    # Deleting a Node at the specified index of LinkedList
+    list.remove_at(2)
+    list.print()
+    print()
+## Searching in LinkedList and length of LinkedList
+    # Printing Length of the LinkedList
+    print("Now Length is",list.length())
+    print()
+    list.Search("Mango")
+    print() 
+    list.Search("Sadiq")
